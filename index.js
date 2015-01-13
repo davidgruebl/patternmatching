16c380ef2f351959dec8c7203d7b6ac8f88bf86d
David Thomas Grübl <me@davidgruebl.com>
2015-01-13 22:49:38 +0100

    feat: rotating face detection

--- index.js
+++ index.js
@@ -1,33 +1,58 @@
 'use strict'
 
-var $ = require('jquery')
-var tracking = require('tracking')
-var face = require('face')
-var eye = require('eye')
-var mouth = require('mouth')
+tracking = require('tracking')
+require('face')
+require('eye')
+require('mouth')
 
 window.onload = function() {
-  var video = document.getElementById('video')
-  var canvas = document.getElementById('canvas')
-  var context = canvas.getContext('2d')
-  var objects = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
+  var container = document.querySelector('.container')
+  var video = document.querySelector('#video')
+  var objects = new tracking.ObjectTracker('face')
   objects.setInitialScale(4)
   objects.setStepSize(2)
   objects.setEdgesDensity(0.1)
+
   tracking.track('#video', objects, {
     camera: true
   })
+
+  var faceX = 0
+  var lastX = 0
+  var faceY = 0
+  var lastY = 0
+  var detected = !1
+
   objects.on('track', function(event) {
-    if (event.data.length === 0) console.log('ligga?')
+    var maxRectArea = 0
+    var maxRect
+
+    detected = event.data.length ? true : false
+
     event.data.forEach(function(rect) {
-      console.log(rect)
-      context.clearRect(0, 0, canvas.width, canvas.height)
-      window.plot(rect.x, rect.y, rect.width, rect.height)
+      if (rect.width * rect.height > maxRectArea){
+        maxRectArea = rect.width * rect.height
+        maxRect = rect
+      }
     })
+
+    if(maxRectArea > 0) {
+      var rectCenterX = maxRect.x + (maxRect.width/2)
+      var rectCenterY = maxRect.y + (maxRect.height/2)
+
+      faceX = Math.floor((360 * (rectCenterX / 320) - 180) * -1)
+      faceY = Math.floor((360 * (rectCenterY / 240) - 180) * -1)
+    }
+
+    rotate(faceX, faceY)
   })
-  window.plot = function(x, y, w, h) {
-    context.strokeStyle = 'pink'
-    context.strokeRect(x, y, w, h)
+
+  requestAnimationFrame(rotate)
+
+  function rotate(timestamp) {
+    if (faceX === lastX && faceY === lastY) return
+    container.style.transform = 'rotateY(' + faceX + 'deg) rotateX(' + faceY + 'deg)'
+    container.style.background = detected ? 'green' : 'red'
+    requestAnimationFrame(rotate)
   }
 }
-
